27115c91c83011d998176a0b5f1d3c0b93865f70
bloody512 <112201928+bloody512@users.noreply.github.com>
2023-02-02 09:53:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,19 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <style>
-    #title {
-        text-align: center;
-    }
-    #graphContainer {
-        text-align: center;
-    }
-    #graph {
-        max-width: 1000px;
-        margin: auto;
-
-    }
-  </style>
+  
 </head>
 
 <body>
